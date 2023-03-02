cd6b651e70feae05abc1dcc6cc5a1375bcc0810c
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-03-02 17:18:57 +1100

    Homescreen Linking
    - Added Linking for Homescreen ImageCarousel

--- components/ImgCarousel.js
+++ components/ImgCarousel.js
@@ -1,23 +1,26 @@
 import Image from 'next/image'
 import React, { useState, useEffect } from 'react'
 import projects from '@/data/projects.json'
+import Link from 'next/link'
 
 const ImgCarousel = ({projectId}) => {
     /* 
-        Rotate Images with description
-
         *** Currently STATIC and using DEMO Data ***
 
         ToDo:
         - setup with CMS Platform
+        - add animation with framer motion
 
      */
 
   return (
     <div className='pt-2 p-5 max-w-sm max-h-sm ease-in duration-300'>
+      <Link href='/blog/[slug]' as={`/blog/${projectId}`}>
         <img src={projects[projectId].images[0]} alt="Image" className='rounded-lg'/>
         <h3 className='text-sm text-left tracking-wide font-semibold pt-1 ease-in duration-300'>{projects[projectId].title} : </h3>
         <p className='text-xs text-left font-thin tracking-wide pt-1 ease-in duration-300'>{projects[projectId].description}</p>
+      </Link>
+        
     </div>
   )
 }
--- components/ImgCarouselDesktop.js
+++ components/ImgCarouselDesktop.js
@@ -1,20 +1,20 @@
 import Image from 'next/image'
 import React, { useState, useEffect } from 'react';
 import projects from '@/data/projects.json'
+import Link from 'next/link';
 
 const ImgCarouselDesktop = ({projectId}) => {
     /* 
-        Rotate Images with description
-
-        *** Currently STATIC and using DEMO Data ***
 
         ToDo:
         - setup with CMS Platform
+        - add animation with framer motion
 
      */
     
       return (
         <div className='grid grid-cols-2'>
+          <Link href='/blog/[slug]' as={`/blog/${projectId}`}>
             <div className='pt-20 p-5'>
                 <img src={projects[projectId].images[0]} alt="Image" className='rounded-lg'/>
             </div>
@@ -23,6 +23,8 @@ const ImgCarouselDesktop = ({projectId}) => {
                 <h3 className='text-sm text-left tracking-wide font-semibold pt-1'>{projects[projectId].title} : </h3>
                 <p className='text-xs text-left font-light tracking-tight pt-1 font-sans'>{projects[projectId].description}</p>
             </div>
+          </Link>
+
         </div>
 
       )
