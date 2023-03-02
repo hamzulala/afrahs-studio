633c828b5d15720d5c997a35440771c8cddecf04
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-03-02 17:30:39 +1100

    Update ImgCarouselDesktop.js
    - Fixed Layout bug For Desktop ImgCarousel

--- components/ImgCarouselDesktop.js
+++ components/ImgCarouselDesktop.js
@@ -5,28 +5,26 @@ import Link from 'next/link';
 
 const ImgCarouselDesktop = ({projectId}) => {
     /* 
+        Rotate Images with description
+
+        *** Currently STATIC and using DEMO Data ***
 
         ToDo:
         - setup with CMS Platform
-        - add animation with framer motion
 
      */
     
       return (
-        <div className='grid grid-cols-2'>
-          <Link href='/blog/[slug]' as={`/blog/${projectId}`}>
-            <div className='pt-20 p-5'>
-                <img src={projects[projectId].images[0]} alt="Image" className='rounded-lg'/>
-            </div>
-            <div className='pt-2 p-5 pr-5'>
-                <img src={projects[projectId].images[1]} alt="Image" className='rounded-lg aspect-square object-cover'/>
-                <h3 className='text-sm text-left tracking-wide font-semibold pt-1'>{projects[projectId].title} : </h3>
-                <p className='text-xs text-left font-light tracking-tight pt-1 font-sans'>{projects[projectId].description}</p>
-            </div>
-          </Link>
-
-        </div>
-
+        <Link href='/blog/[slug]' as={`/blog/${projectId}`} className='grid grid-cols-2'>
+          <div className='pt-20 p-5'>
+              <img src={projects[projectId].images[0]} alt="Image" className='rounded-lg'/>
+          </div>
+          <div className='pt-2 p-5 pr-5'>
+              <img src={projects[projectId].images[1]} alt="Image" className='rounded-lg aspect-square object-cover'/>
+              <h3 className='text-sm text-left tracking-wide font-semibold pt-1'>{projects[projectId].title} : </h3>
+              <p className='text-xs text-left font-light tracking-tight pt-1 font-sans'>{projects[projectId].description}</p>
+          </div>
+        </Link>
       )
 }
 
