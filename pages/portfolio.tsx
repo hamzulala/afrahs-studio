356d3c3f1013b78dc46332b50c6f398700ace157
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-03-04 11:38:59 +1100

    Updated Projects.json
    - Updated Demodata for Testing
    - Put some better colours for the Portfolio.tsx page

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -7,13 +7,14 @@ import projects from '@/data/projects.json'
 type Props = {}
 
 const portfolio = (props: Props) => {
+
   return (
-    <div className='flex  items-center justify-center h-screen bg-[#F87060]'>
-        <Header primaryColor={'#F87060'} secondaryColor={'#102542'}/>
+    <div className='flex  items-center justify-center h-screen bg-[#B3A394]'>
+        <Header primaryColor={'#B3A394'} secondaryColor={'#102542'}/>
         {/*
          * Should be a dynamic list of all projects
          */}
-        <div className='absolute columns-2 md:columns-3 lg:columns-4 p-4 top-20 bg-[#F87060]'>
+        <div className='absolute columns-2 md:columns-3 lg:columns-4 p-4 top-20 bg-[#B3A394]'>
           {projects.map((project) => (
             <ProjectImgLink key={project.id} project={project}/>
           ))}
