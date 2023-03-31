aea77705dbe92d71fc2c2caed8387cd98c73dce9
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-03-31 21:23:41 +1100

    Update fetchProjects.ts

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,12 +1,13 @@
 import { Project } from '../typings'
 
 export const fetchProjects = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)
-
+    //const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)
+    const res = await fetch(`https://afrahs-studio.vercel.app/api/getProjects`)
     const data = await res.json()
+    //console.log("data", data)
     const project: Project[] = data.project
 
-    console.log("fetching", project)
+    //console.log("fetching", project)
 
     return project
 }
