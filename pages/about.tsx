cd4c5066278d3820400ec92dd723ee1f44705a73
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-03-31 15:58:30 +1100

    build test 2

---
+++ pages/about.tsx
@@ -0,0 +1,24 @@
+import Header from "@/components/Header"
+import React from "react"
+import Origin from "@/components/Origin"
+import AwardsList from "@/components/AwardsList"
+import AboutMe from "@/components/AboutMe"
+
+type Props = {};
+
+//need to fix this page and add a fade effect to the map
+
+const About = (props: Props) => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Header />
+      <Origin/>
+      <div className="flex flex-row space-x-10">
+        <AwardsList/>
+        <AboutMe/>
+      </div>
+    </div>
+  );
+};
+
+export default About;
