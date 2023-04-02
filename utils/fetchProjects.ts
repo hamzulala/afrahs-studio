1906156dcaa775dd37905834358f2e634eb10451
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-04-02 18:15:04 +1000

    Update fetchProjects.ts

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -2,7 +2,9 @@ import { Project } from '../typings'
 
 export const fetchProjects = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)
-    const data = await res.json()
+    const text = await res.text()
+    console.log(text)
+    const data = JSON.parse(text)
     
     const project: Project[] = data.project
 
