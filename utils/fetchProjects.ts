2e6a7d5c9602d0035d8d45e6c50d4c85ca52ca12
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-04-04 00:49:43 +1000

    Update fetchProjects.ts

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,12 +1,18 @@
 import { Project } from "../typings";
 
 export const fetchProjects = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
-    const data = await res.json()
-    const project: Project[] = data.project;
+    try {
+        
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
+        const data = await res.json()
+        const project: Project[] = data.project;
+    
+        console.log("fetching", project);
+        return project;
 
-    console.log("fetching", project);
-    return project;
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 /**
