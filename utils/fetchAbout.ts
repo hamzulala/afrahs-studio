25fc19e2a00090c9e68ded99029bdf4a6be8c362
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-04-04 00:19:49 +1000

    Deployment test 2

--- utils/fetchAbout.ts
+++ utils/fetchAbout.ts
@@ -1,8 +1,8 @@
 import { About } from '../typings'
 
 export const fetchAbout = async () => {
-    const res = await fetch(`https://afrahs-studio.vercel.app/api/getAbout`)
-    //const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getAbout`)
+    //const res = await fetch(`https://afrahs-studio.vercel.app/api/getAbout`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getAbout`)
     
     const data = await res.json()
     const about: About = data.about
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -2,9 +2,7 @@ import { Project } from '../typings'
 
 export const fetchProjects = async () => {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)
-    const text = await res.text()
-    console.log(text)
-    const data = JSON.parse(text)
+    const data = await res.json()
     
     const project: Project[] = data.project
 
