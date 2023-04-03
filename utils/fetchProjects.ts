d115e9d2cfd7ddfbdb9212ef3552a2ce47ec887b
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-04-04 00:56:30 +1000

    Update fetchProjects.ts

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -5,7 +5,7 @@ export const fetchProjects = async() => {
         
         const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
         const data = await res.json()
-        const project: Project[] = data.project;
+        const project: Project[] = data?.project;
     
         console.log("fetching", project);
         return project;
